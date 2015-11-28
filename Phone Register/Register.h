3cacad78cb80248e2a6042525087b7e6fcb9ea85
chiunti <chiunti_abc@hotmail.com>
2015-11-27 21:46:26 -0600

    First commit

---
+++ Phone Register/Register.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Phone Register
+//
+//  Created by admin on 27/11/15.
+//  Copyright © 2015 chiuntisoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Register : UIViewController
+
+
+@end
+
---
+++ Phone Register/Register.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Phone Register
+//
+//  Created by admin on 27/11/15.
+//  Copyright © 2015 chiuntisoft. All rights reserved.
+//
+
+#import "Register.h"
+
+@interface Register ()
+
+@end
+
+@implementation Register
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
